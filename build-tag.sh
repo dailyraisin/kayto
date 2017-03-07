ff0c6a7b794365a04a27fe386f95b4032c0c45b4
Joe K <joe@dailyraisin.com>
2017-03-07 16:33:16 -0500

    bump and \n

--- build-tag.sh
+++ build-tag.sh
@@ -6,8 +6,8 @@ git checkout -b tmp && \
   gulp build && \
   git add -f dist && \
   git commit && \
-  echo "\n\nNow git tag -a vX.X.X\n" && \
-  echo "\ngit push origin vX.X.X\n" && \
-  echo "\nnpm publish\n" && \
-  echo "\ngit checkout master\n" && \
-  echo "\ngit branch -D tmp\n"
+  echo -n "\nNow git tag -a vX.X.X\n" && \
+  echo -n "\ngit push origin vX.X.X\n" && \
+  echo -n "\nnpm publish\n" && \
+  echo -n "\ngit checkout master\n" && \
+  echo -n "\ngit branch -D tmp\n"
